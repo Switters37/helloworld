56b6a15ee83139708bbd3fd57b8353e41834c1d4
Mike <Mike@131.202.167.19>
2014-07-14 14:34:28 -0300

    If then stuff.

---
+++ ifTrial.py
@@ -0,0 +1,26 @@
+davesage = 23
+jeffsage = 28
+
+if davesage > jeffsage:
+    print 'Dave is older than Jeff'
+    
+if jeffsage > davesage:
+    print 'Hot damn! Jeff is older than Dave!'
+    
+    
+    
+    
+if davesage > jeffsage:
+    print 'Dave is older'
+else:
+    print "jeff is older"
+    
+if davesage > jeffsage:
+    print "dave is older"
+elif jeffsage > davesage:
+    print "jeff is older"
+else:
+    print "what the hell!?"
+     
+        
+        
