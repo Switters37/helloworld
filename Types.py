dac4d88d70f96acabf58872ab347518df5de0c2a
Mike <Mike@131.202.167.19>
2014-07-14 13:57:36 -0300

    More stuff

---
+++ Types.py
@@ -0,0 +1,21 @@
+import math
+import numpy as np
+
+x = 10
+y = 3
+
+print x + y
+print x-y
+print x*y
+print x**2
+print x/y 
+print x**y 
+print float (x)/y
+radius = 2
+print np.pi
+print "area - ", math.pi*radius**2
+print math.cos(np.pi)
+
+
+
+
